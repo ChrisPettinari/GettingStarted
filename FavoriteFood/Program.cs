0b3cca64ff2a519b672316a4079f4a3c640109fb
ChrisPettinari <chrispettinari@gmail.com>
2017-08-31 11:26:44 -0400

    fav food B

---
+++ FavoriteFood/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Meep meep");
+
+            var ans = Console.ReadLine();
+
+            if (ans == "pizza")
+            {
+                Console.WriteLine("Aw ye i luh dat boneless");
+
+            }
+            else
+            {
+                Console.WriteLine("nah B");
+            }
+
+
+            
+        }
+    }
+}
