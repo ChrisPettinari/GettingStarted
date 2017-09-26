f7da427bab64762f7c6524a88e6384cbcbe2d1ef
ChrisPettinari <31513015+ChrisPettinari@users.noreply.github.com>
2017-09-26 19:56:12 -0400

    uh iss ya boi

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+
+            
+            Console.Write("Enter an expression: ");
+
+            string expression = Console.ReadLine();
+
+            string[] exp = expression.Split(' ');
+
+            int solution;
+            if (exp[1] == "+")
+            {
+                solution = Convert.ToInt32(exp[0]) + Convert.ToInt32(exp[2]);
+
+                Console.WriteLine("Result: " + expression + " = " + solution);
+            }
+
+            if (exp[1] == "-")
+            {
+                solution = Convert.ToInt32(exp[0]) - Convert.ToInt32(exp[2]);
+
+                Console.WriteLine("Result: " + expression + " = " + solution);
+            }
+
+            if (exp[1] == "*")
+            {
+                solution = Convert.ToInt32(exp[0]) * Convert.ToInt32(exp[2]);
+
+                Console.WriteLine("Result: " + expression + " = " + solution);
+            }
+
+                if (exp[1] == "/")
+                {
+                    solution = Convert.ToInt32(exp[0]) / Convert.ToInt32(exp[2]);
+
+                    Console.WriteLine("Result: " + expression + " = " + solution);
+                }
+            }
+        }
+    }
+}
